417db4813dbc7bfa3085b1fad79d45f3bb302505
Kwasi Dankwa <edankwa14@gmail.com>
2022-06-21 21:16:36 -0400

    fixing dependencies issue

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //Adding Dependencies needed - inquirer,mysql2,console.table packages
 const inquirer = require("inquirer");
 const mysql = require('mysql2');
-const table = require('console.table');
+require('console.table');
 require('dotenv').config()
 
 //Create the connection to database
