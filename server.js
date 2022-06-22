fb81fe0506cc136eacc33484cae655f7c542ac0d
Kwasi Dankwa <edankwa14@gmail.com>
2022-06-21 20:32:32 -0400

    install dotenv npm package

--- server.js
+++ server.js
@@ -2,13 +2,16 @@
 const inquirer = require("inquirer");
 const mysql = require('mysql2');
 const table = require('console.table');
+require('dotenv').config()
 
 //Create the connection to database
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'user',
-    password: '042698KWes!'
+    password: '',
     database: 'employees_db'
 });
+
+
 
 
